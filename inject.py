82b043a375f912b8d1f48fedae2cafa4515cab73
Noah Aliaga <noahjeremias31@icloud.com>
2024-12-17 07:44:26 -0500

    Organize the Python script further

--- inject.py
+++ inject.py
@@ -2,23 +2,6 @@ import sys
 import os
 import subprocess
 
-
-def getPlatformCommands() -> tuple:
-    copyCommand = ""
-    deleteCommand = ""
-
-    if (sys.platform == "win32"):
-        copyCommand = "copy"
-        deleteCommand = "del"
-    else:
-        copyCommand = "cp"
-        deleteCommand = "rm"
-    return copyCommand, deleteCommand
-
-
-copyCommand, deleteCommand = getPlatformCommands()
-
-
 hookAddress = hex(0x808D120C)
 
 dolLocation = os.path.join("DOL", "main.dol")
@@ -39,36 +22,91 @@ linkerFlagsString = " ".join([*linkerFlags])
 hookFile = os.path.join("hook.s")
 
 codeFiles = [
-    os.path.join("RemoveAbility.cc"),
-    os.path.join("DisableInvincible.cc")
+    os.path.join("src", "RemoveAbility.cc"),
+    os.path.join("src", "DisableInvincible.cc")
 ]
 
 objectFiles = []
 
-print("[1/4]: Creating code hook...")
-subprocess.run(f"powerpc-eabi-as {hookFile} -o hook.o", shell=True)
+# Utility functions
 
-print("[2/4]: Compiling source files...")
-for sourceFiles in codeFiles:
+
+def getPlatformCommands() -> tuple:
+    copyCommand = ""
+    deleteCommand = ""
+
+    if (sys.platform == "win32"):
+        copyCommand = "copy"
+        deleteCommand = "del"
+    else:
+        copyCommand = "cp"
+        deleteCommand = "rm"
+    return copyCommand, deleteCommand
+
+
+def splitExtension(file: str):
+    baseName = os.path.basename(file)
+    pre, ext = os.path.splitext(baseName)
+    return pre
+
+
+copyCommand, deleteCommand = getPlatformCommands()
+
+
+def createCodeHook():
+    print("[1/4]: Creating code hook...")
     subprocess.run(
-        f"powerpc-eabi-g++ {sourceFiles} {compilerFlagsString}", shell=True)
+        f"powerpc-eabi-as {hookFile} -o hook.o", shell=True
+    )
 
-print("[3/4]: Linking object files...")
-subprocess.run("mkdir build", shell=True)
 
-for sourceFiles in codeFiles:
-    objects = os.path.basename(sourceFiles)
-    pre, ext = os.path.splitext(objects)
-    objectFiles.append(f"{pre}.o")
+def compileSourceFiles():
+    print("[2/4]: Compiling source files...")
 
-objectFilesString = " ".join([*objectFiles])
-subprocess.run(f"powerpc-eabi-ld hook.o {linkerFlagsString} {
-               objectFilesString} -o build/linked_output.elf", shell=True)
-subprocess.run(f"{deleteCommand} hook.o {objectFilesString}", shell=True)
+    for sourceFiles in codeFiles:
+        subprocess.run(
+            f"powerpc-eabi-g++ {sourceFiles} {compilerFlagsString}", shell=True
+        )
 
-print("[4/4]: Patching ELF into DOL file...")
-subprocess.run(f"{copyCommand} {dolLocation} build", shell=True)
-subprocess.run(
-    f"wit dolpatch build/main.dol new=text,AUTO xml={patchXML} entry={hookAddress} --source .", shell=True)
 
-print("Done :)")
+def linkObjectFiles():
+    print("[3/4]: Linking object files...")
+
+    if os.path.join("build") is False:
+        subprocess.run(
+            "mkdir build", shell=True
+        )
+
+    for sourceFiles in codeFiles:
+        prefix = splitExtension(sourceFiles)
+        objectFiles.append(f"{prefix}.o")
+
+    objectFilesString = " ".join([*objectFiles])
+    subprocess.run(
+        f"powerpc-eabi-ld hook.o {linkerFlagsString} {objectFilesString} -o build/linked_output.elf", shell=True
+    )
+    subprocess.run(
+        f"{deleteCommand} hook.o {objectFilesString}", shell=True
+    )
+
+
+def patchELFIntoDOL():
+    print("[4/4]: Patching ELF into DOL file...")
+
+    subprocess.run(
+        f"{copyCommand} {dolLocation} build", shell=True
+    )
+    subprocess.run(
+        f"wit dolpatch build/main.dol new=text,AUTO xml={patchXML} entry={hookAddress} --source .", shell=True
+    )
+
+
+def injectMain():
+    createCodeHook()
+    compileSourceFiles()
+    linkObjectFiles()
+    patchELFIntoDOL()
+    print("Done :)")
+
+
+injectMain()
