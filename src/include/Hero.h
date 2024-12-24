bb6c210eb259ec5109bc70c635c2337f354f12af
Noah Aliaga <noahjeremias31@icloud.com>
2024-12-24 11:05:57 -0500

    Losing my mind over here

--- src/include/Hero.h
+++ src/include/Hero.h
@@ -2,7 +2,6 @@
 #define HERO_H_
 
 #include "Ability.h"
-#include "Array.h"
 #include "Component.h"
 #include "Invincible.h"
 
@@ -10,9 +9,13 @@ typedef struct Hero
 {
     u8 x0[0x67 - 0x0];
     u8 mDisplayHeroModel; // x67
-    u8 x68[0x18C - 0x68];
+    u8 x68[0x11C - 0x68];
+    ExplicitScopedPointer<Ability> mAbilityData; // x11C
+    ExplicitScopedPointer<Invincible> mInvincibleData; // x124
+    u8 x12C[0x18C - 0x12C];
     ExplicitScopedPointer<u32> mUnkPtr; // x18C
 
+    Ability * getAbilityData(); //
     Invincible * getInvincData(); // 804EDB1C
 } Hero;
 
