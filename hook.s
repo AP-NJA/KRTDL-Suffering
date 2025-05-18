c51fd6179db9368f85c7d5055bbc707e3e00fc0f
Noah Aliaga <noahjeremias31@icloud.com>
2025-05-18 00:31:39 -0400

    Change entry point for mod to fit new changes

--- hook.s
+++ hook.s
@@ -1,12 +1,8 @@
 .global HOOK
 HOOK:
-    lis %r3, (_Z8mainHookv+0x48000001-0x804e9f44)@ha
-    addi %r3, %r3, (_Z8mainHookv+0x48000001-0x804e9f44)@l
-    lis %r4, 0x804F
-    stw %r3, -0x60BC (%r4)
+    lis %r3, (_Z8mainHookv+0x48000001-0x8035dae4)@ha
+    addi %r3, %r3, (_Z8mainHookv+0x48000001-0x8035dae4)@l
+    lis %r4, 0x8036
+    stw %r3, -0x251C (%r4)
 
-   # lis %r3, (_Z11disableHookv+0x48000001-0x804f054c)@ha
-   # addi %r3, %r3, (_Z11disableHookv+0x48000001-0x804f054c)@l
-   # lis %r4, 0x804F
-   # stw %r3, 0x054C (%r4)
     b start
